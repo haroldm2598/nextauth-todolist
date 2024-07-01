434aec0b8367a1c9996dc12c9166bf4b360e2471
Harold <cvuyante24@gmail.com>
2024-07-01 21:01:02 +0800

    save

--- middleware.ts
+++ middleware.ts
@@ -4,43 +4,46 @@ import { getToken } from 'next-auth/jwt';
 import { withAuth } from 'next-auth/middleware';
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
-export default async function middleware(
-	req: NextRequest,
-	event: NextFetchEvent
-) {
-	const token = await getToken({ req });
-	const isAuthenticated = !!token;
 
-	// error finding sira naman yung button ng sign-up nag redirect sa sign in lagi
-	if (req.nextUrl.pathname.startsWith('/sign-in') && isAuthenticated) {
-		return NextResponse.redirect(new URL('/admin', req.url));
-	}
+export default async function middleware(req: NextRequest, event:NextFetchEvent) {
+    const token = await getToken({ req });
+    const isAuthenticated = !!token;
 
-	if (req.nextUrl.pathname.startsWith('/sign-up') && isAuthenticated) {
-		return NextResponse.redirect(new URL('/admin', req.url));
-	}
+    // if the user is already authenticated and tries to access one of the auth pages
+    // redirect to the home page
+    if (
+        (req.nextUrl.pathname.startsWith("/sign-in") ||
+            req.nextUrl.pathname.startsWith("/sign-up")) &&
+        isAuthenticated
+    ) {
+        return NextResponse.redirect(new URL("/admin", req.url));
+    }
 
-	if (
-		req.nextUrl.pathname.startsWith('/') ||
-		req.nextUrl.pathname.startsWith('/sign-in') ||
-		req.nextUrl.pathname.startsWith('/sign-up')
-	) {
-		return fetch(req);
-	}
+    // if the user is not authenticated, allow them to visit the auth pages
+    if (
+        req.nextUrl.pathname.startsWith("/sign-in") ||
+        req.nextUrl.pathname.startsWith("/sign-up")
+    ) {
+        return fetch(req);
+    }
 
-	const authMiddleware = await withAuth({
-		pages: {
-			signIn: `/sign-in`
-		}
-	});
-
+    // if the user is not authenticated and tries to access any other page
+    // redirect to the login page
+    const authMiddleware = withAuth({
+        pages: {
+            signIn: `/sign-in`
+        }
+    });
+	
 	// @ts-expect-error
-	return authMiddleware(req, event);
+    return authMiddleware(req, event);
 }
 
+// config to match all pages
 export const config = {
-	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
+    matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"]
 };
+
 
 // export { default } from 'next-auth/middleware';
 // export const config = {
